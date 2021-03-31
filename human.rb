9638e39318c8f9cf8bbbdd3777c575b76799b194
Kalon Hinds <kjhinds@gmail.com>
2021-03-30 20:49:12 -0700

    Hide secret code in terminal when picked by player

--- human.rb
+++ human.rb
@@ -1,3 +1,5 @@
+require 'io/console'
+
 class Human < Player
   def make_guess
     ask_guess
@@ -12,12 +14,29 @@ class Human < Player
 
   def pick_secret
     ask_secret_number
-    response = gets.chomp
+    response = $stdin.noecho(&:gets).chomp
     if valid_secret_number?(response)
       response
     else
       invalid_secret_number
       pick_secret
     end
+  end
+
+  def valid_secret_number?(string)
+    string.length == 4 && string.chars.all? { |c| c.match?(/[1-8]/) }
+  end
+
+  def ask_guess
+    puts "It's #{@name}'s turn."
+    puts 'Take a guess!'
+  end
+
+  def ask_secret_number
+    puts 'Pick a secret number with 4 digits using numbers 1 - 8 (Code will be hidden).'
+  end
+
+  def invalid_secret_number
+    puts 'Invalid number!  Must be: ####  Do not include 0s or 9s.'
   end
 end
--- player.rb
+++ player.rb
@@ -11,21 +11,4 @@ class Player
     @name = name
     @number = number
   end
-
-  def valid_secret_number?(string)
-    string.length == 4 && string.chars.all? { |c| c.match?(/[1-8]/) }
-  end
-
-  def ask_guess
-    puts "It's #{@name}'s turn."
-    puts 'Take a guess!'
-  end
-
-  def ask_secret_number
-    puts 'Pick a secret number with 4 digits using numbers 1 - 8.'
-  end
-
-  def invalid_secret_number
-    puts 'Invalid number!  Must be: ####  Do not include 0s or 9s.'
-  end
 end
